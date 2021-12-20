d2f4fd2042f30d7da84069772fc0ea7cb4e20cbd
Victoriya Gilenko <victoriyagilenko@gmail.com>
2021-12-20 22:45:35 +0100

    добавляет изменения

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Profile from './Components/Profile';
+import user from './Components/user.json';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
